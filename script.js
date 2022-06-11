79c24a603275ca8421d9f313277d7b2f6752e53c
Roy Romero <Romerorr94@gmail.com>
2022-06-11 18:13:47 -0400

    currently working on all functions weaving together properly

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-var initial = [];
 var secondary = [];
 var initialNumber;
 var secondaryNumber;
+var equalsPressed = false;
 var result;
 var operation;
 
-
+// to do result function not being stored as array
+// decimals needs work in
+// screen needs to strech upwards
 function addArray(a){
+
+
 
     secondary.push(a);
 
-    secondaryNumber = Number(secondary.join(""));
+    if (secondary.includes(".") == true) {
 
+
+
+    }
+
+
+    secondaryNumber = Number(secondary.join(""));
+        
 }
 
 function deleteLast(){
@@ -25,12 +36,15 @@ function deleteLast(){
 
 function equalsOperation(){
 
-    if (initialNumber != null){
+    if (initialNumber != null && secondaryNumber != null){
 
         if(operation == "+"){
             result = initialNumber + secondaryNumber;
-            initialScreen.textContent = initialNumber + " + " + secondaryNumber + " = ";
+            initialScreen.textContent = initialNumber + " + " + secondaryNumber + " = ";   
+            secondary = result.toString().split('').map(iNum => parseInt(iNum, 10));
+            console.log(secondary)
             secondaryScreen.textContent = result;
+            
         }
         else if(operation == "-"){
             result = initialNumber - secondaryNumber;
@@ -42,37 +56,65 @@ function equalsOperation(){
             initialScreen.textContent = initialNumber + " ÷ " + secondaryNumber + " = ";
             secondaryScreen.textContent = result;
         }
-        else if(operation == "*"){
+        else if(operation == "×"){
             result = initialNumber * secondaryNumber;
             initialScreen.textContent = initialNumber + " × " + secondaryNumber + " = ";
             secondaryScreen.textContent = result;
         }
 
+        equalsPressed = true;
 
+    }   
 
-
-    }
-
-   
     
 }
 
-function checkoperation(){
-    if (operation == null) {
-        initialNumber = secondaryNumber
-        secondaryNumber = 0;
+function checkEquals(){ // function that checks if equals button was pressed and if so next number that is pressed 
+                        // resets
+    if(equalsPressed == true){
+        clearAll();
+        equalsPressed = false;
+
+    }
+
+}
+
+function continueOperation(){ //Function that lets the calculator continue the operations
+    initialNumber = result;
+    secondaryNumber = null;
+    secondary = [];
+    equalsPressed = false;
+}
+
+function checkoperation(){                // function that checks if the operation variable is null
+    if (operation == null) {              // If operation is null second value is transferred to first value and is solidified
+        initialNumber = secondaryNumber   // Then secondary value is reset so user can add second value in their operation.
+        secondaryNumber = null;
         secondary = [];
     }
 
-    if(operation != null && initialNumber != null){
+    if(operation != null && secondaryNumber != null){
+        equalsOperation();
+        continueOperation();
+        initialScreen.textContent = initialNumber + " " + operation;
 
+       
     }
+}
+
+
+function changeValues(){
+    if(operation != null && secondaryNumber == null){
+        initialScreen.textContent = initialNumber + " " + operation;
+        secondaryScreen.textContent = "0";    
+        
+    }
+
 }
 
 
 function clearAll(){ //Function clears both screens
     secondary = [];
-    initial = [];
     initialNumber = null;
     secondaryNumber = 0;
     operation = null;
@@ -92,6 +134,7 @@ const Button7 = document.getElementById('Button7');
 const Button8 = document.getElementById('Button8');
 const Button9 = document.getElementById('Button9');
 
+const decimalButton = document.getElementById('decimalButton')
 const divideButton = document.getElementById('divideButton')
 const multiplyButton = document.getElementById('multiplyButton')
 const plusButton = document.getElementById('plusButton')
@@ -102,7 +145,7 @@ const equalsButton = document.getElementById('equalsButton')
 
 const initialScreen = document.getElementById('initialScreen') // Initial screen is first screen from the top
 const secondaryScreen = document.getElementById('secondaryScreen') // Secondary screen is the the second screen
-                                                                   // From the top
+                                                                   // from the top
 const clearButton = document.getElementById('clearButton')
 const deleteButton = document.getElementById('deleteButton')
 
@@ -114,58 +157,76 @@ Button0.addEventListener("click", ()=>{
 });
 
 Button1.addEventListener("click", ()=>{
+    checkEquals();
     addArray(1);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button2.addEventListener("click", ()=>{
+    checkEquals();
     addArray(2);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button3.addEventListener("click", ()=>{
+    checkEquals();
     addArray(3);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button4.addEventListener("click", ()=>{
+    checkEquals();
     addArray(4);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button5.addEventListener("click", ()=>{
+    checkEquals();
     addArray(5);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button6.addEventListener("click", ()=>{
+    checkEquals();
     addArray(6);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button7.addEventListener("click", ()=>{
+    checkEquals();
     addArray(7);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button8.addEventListener("click", ()=>{
+    checkEquals();
     addArray(8);
     secondaryScreen.textContent = secondaryNumber;
 
 });
 
 Button9.addEventListener("click", ()=>{
+    checkEquals();
     addArray(9);
     secondaryScreen.textContent = secondaryNumber;
 
 });
+
+
+decimalButton.addEventListener("click", ()=>{
+    addArray(".");
+    secondaryScreen.textContent = secondaryNumber;
+
+});
+
+
 
 
 
@@ -185,13 +246,7 @@ plusButton.addEventListener("click", ()=>{
 
     checkoperation();
     operation = "+";
-    initialScreen.textContent = initialNumber + " +";
-    secondaryScreen.textContent = secondaryNumber;
-
-
-    
-
-
+    changeValues();
 });
 
 
@@ -199,9 +254,7 @@ minusButton.addEventListener("click", ()=>{
 
     checkoperation();
     operation = "-";
-    initialScreen.textContent = initialNumber + " -";
-    secondaryScreen.textContent = secondaryNumber;
-
+    changeValues();
 
 });
 
@@ -209,30 +262,21 @@ minusButton.addEventListener("click", ()=>{
 multiplyButton.addEventListener("click", ()=>{
 
     checkoperation();
-    operation = "*";
-    initialScreen.textContent = initialNumber + " ×";
-    secondaryScreen.textContent = secondaryNumber;
+    operation = "×";
+    changeValues();
 
 
 });
-
-
-
 
 divideButton.addEventListener("click", ()=>{
 
     checkoperation();
     operation = "÷";
-    initialScreen.textContent = initialNumber + " ÷";
-    secondaryScreen.textContent = secondaryNumber;
-
+    changeValues();
 
 });
-
 
 equalsButton.addEventListener("click", ()=>{
 
     equalsOperation();
-
-
 });
